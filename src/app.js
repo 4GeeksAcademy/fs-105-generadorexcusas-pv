ffec85f1b778e83be805427fd12bd019d3b76a89
Pedro Valencia Alves <pedrovalenciaalves98@gmail.com>
2025-03-14 18:50:16 +0000

    Generador de excusas terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function generador(){
+  let whoExcusa = Math.floor (Math.random()* who.length)
+  let actionExcusa = Math.floor (Math.random()* action.length)
+  let whatExcusa = Math.floor (Math.random()* what.length)
+  let whenExcusa = Math.floor (Math.random()* when.length)
+ return who[whoExcusa] + " " + action[actionExcusa] + " " + what[whatExcusa] + " " + when[whenExcusa]
+}
+  let excuseElement = document.getElementById('excuse');
+    excuseElement.innerHTML = generador();
+
   console.log("Hello Rigo from the console!");
 };
+
